0dd927f7d4076d4d0e31fcfa5868c1c86de4e0d5
Prince Agezi .N <princeagezinweke@gmail.com>
2024-08-18 15:38:48 +0100

    Update: Logo, Favicon, Brand name.

--- js/script.js
+++ js/script.js
@@ -1,37 +1,42 @@
 $(function () {
-    $('.nav-btn').on('click', function () {
-        $(this).toggleClass('open');
-    });
+  $(".nav-btn").on("click", function () {
+    $(this).toggleClass("open");
+  });
 
-    var currentIndex = 0;
-    var $backgroundImage = $(".image-infinite-bg");
-    var images = $backgroundImage.data('images');
+  var currentIndex = 0;
+  var $backgroundImage = $(".image-infinite-bg");
+  var images = $backgroundImage.data("images");
 
-    function changeBackgroundImage() {
-        // Mengubah gambar latar belakang dengan indeks berikutnya dalam array
-        $backgroundImage.removeClass('animation-bg');
-        currentIndex = (currentIndex + 1) % images.length;
-        var imagePath = images[currentIndex];
-        $backgroundImage.css("background-image", "url('" + imagePath + "')");
-        $backgroundImage[0].offsetHeight;
-        $backgroundImage.addClass('animation-bg');
+  function changeBackgroundImage() {
+    // Mengubah gambar latar belakang dengan indeks berikutnya dalam array
+    $backgroundImage.removeClass("animation-bg");
+    currentIndex = (currentIndex + 1) % images.length;
+    var imagePath = images[currentIndex];
+    $backgroundImage.css("background-image", "url('" + imagePath + "')");
+    $backgroundImage[0].offsetHeight;
+    $backgroundImage.addClass("animation-bg");
+  }
 
-    }
+  // Panggil fungsi untuk pertama kali
+  changeBackgroundImage();
 
-    // Panggil fungsi untuk pertama kali
-    changeBackgroundImage();
-
-    // Set interval untuk memanggil fungsi perubahan gambar dengan waktu yang ditentukan
-    setInterval(changeBackgroundImage, 5000); // 10 detik (sesuaikan dengan waktu animasi CSS di atas)
+  // Set interval untuk memanggil fungsi perubahan gambar dengan waktu yang ditentukan
+  setInterval(changeBackgroundImage, 5000); // 10 detik (sesuaikan dengan waktu animasi CSS di atas)
 });
 
-$(window).ready(function() {
-    $(window).scroll(function() {
-      var scroll = $(window).scrollTop();
-       if (scroll >100) {
-        $("#header").addClass('glass-effect');
-       } else {
-        $("#header").removeClass("glass-effect");
-       }
-     })
-   })
+$(window).ready(function () {
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > 100) {
+      $("#header").addClass("glass-effect");
+    } else {
+      $("#header").removeClass("glass-effect");
+    }
+  });
+});
+
+// window.onload = function () {
+//   // Update the brand logo
+//   const logoImg = document.querySelector(".logo-container > img");
+//   if (logoImg) logoImg.src = "image/brand-logo/logo.png";
+// };
